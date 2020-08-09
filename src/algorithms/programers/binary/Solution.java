4e9f7eb7d5c622ee5e3efbdc0089150c7a7cc689
ndgndg91 <ndgndg91@gmail.com>
2020-08-09 19:56:06 +0900

    <feat> same bitCount but bigger N

---
+++ src/algorithms/programers/binary/Solution.java
@@ -0,0 +1,23 @@
+package algorithms.programers.binary;
+
+public class Solution {
+
+    public static void main(String[] args) {
+        int n = 15;
+        int biggerNWithSameBigCount = getBiggerNWithSameBigCount(n);
+        System.out.println(biggerNWithSameBigCount);
+    }
+
+    private static int getBiggerNWithSameBigCount(int n){
+        int bitCount = Integer.bitCount(n);
+        int answer = n;
+        for (int i = n+1; i < n * 2; i++) {
+            if (bitCount == Integer.bitCount(i)) {
+                answer = i;
+                break;
+            }
+        }
+
+        return answer;
+    }
+}
