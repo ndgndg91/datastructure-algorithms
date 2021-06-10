aaeff51cb00f2cdac1b1b7b402aeb43e716dfc11
ndgndg91 <ndgndg91@gmail.com>
2021-06-10 21:09:46 +0900

    <feat> code wars smile faces

---
+++ src/algorithms/codewars/SmileFaces.java
@@ -0,0 +1,48 @@
+package algorithms.codewars;
+
+import org.junit.jupiter.api.Test;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.regex.Pattern;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+public class SmileFaces {
+
+
+    public static int countSmileys(List<String> arr) {
+        // Just Smile :)
+        final var smile = Pattern.compile("^[;|:][-|~]?[)|D]$");
+        long count = arr.stream().filter(s -> smile.matcher(s).find()).count();
+        return (int) count;
+    }
+
+    @Test
+    public void test1() {
+        List<String> a = new ArrayList<>();
+        a.add(":)"); a.add(":D"); a.add(":-}"); a.add(":-()");
+        assertEquals(2, SmileFaces.countSmileys(a));
+    }
+
+    @Test
+    public void test2() {
+        List<String> a = new ArrayList<>();
+        a.add(":)"); a.add("XD"); a.add(":0}"); a.add("x:-"); a.add("):-"); a.add("D:");
+        assertEquals(1, SmileFaces.countSmileys(a));
+    }
+
+    @Test
+    public void test4() {
+        List<String> a = new ArrayList<>();
+        a.add(":)"); a.add(":D"); a.add("X-}"); a.add("xo)"); a.add(":X"); a.add(":-3"); a.add(":3");
+        assertEquals(2, SmileFaces.countSmileys(a));
+    }
+
+    @Test
+    public void test5() {
+        List<String> a = new ArrayList<>();
+        a.add(":)"); a.add(":)"); a.add("x-]"); a.add(":ox"); a.add(";-("); a.add(";-)"); a.add(";~("); a.add(":~D");
+        assertEquals(4, SmileFaces.countSmileys(a));
+    }
+}
