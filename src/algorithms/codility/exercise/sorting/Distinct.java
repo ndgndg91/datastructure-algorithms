9680bd91e339f69f9fe52550827895f768e95def
ndgndg91 <ndgndg91@gmail.com>
2022-01-22 13:01:59 +0900

    <feat> codility Dinstinct

---
+++ src/algorithms/codility/exercise/sorting/Distinct.java
@@ -0,0 +1,21 @@
+package algorithms.codility.exercise.sorting;
+
+import java.util.HashSet;
+
+public class Distinct {
+
+    public static void main(String[] args) {
+        final var distinct = new Distinct();
+        int solution = distinct.solution(new int[]{1, 1, 3, 3, 4, 4, 2});
+        System.out.println(solution);
+    }
+
+    public int solution(int[] A) {
+        final var s = new HashSet<Integer>();
+        for (var v : A) {
+            s.add(v);
+        }
+
+        return s.size();
+    }
+}
