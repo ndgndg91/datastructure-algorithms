ebca33c003daeedc4d960f65a03bb418b104cd20
ndgndg91 <ndgndg91@gmail.com>
2022-01-22 16:42:20 +0900

    <feat> codility ya

---
+++ src/algorithms/codingtest/ya/InverterTest.java
@@ -0,0 +1,14 @@
+package algorithms.codingtest.ya;
+
+import org.junit.jupiter.api.Test;
+
+public class InverterTest {
+
+    @Test
+    public void shouldInvert() {
+//        String inverted = Inverter.invert(null);
+//
+//        Assertions.asser
+    }
+
+}
