6eaabd619fb5c3584a281bb1576081536aaa8641
ndgndg91 <ndgndg91@gmail.com>
2022-02-03 15:41:19 +0900

    <feat> my double linked list

---
+++ src/datastructure/list/udemy/EmployeeDoubleLinkedList.java
@@ -0,0 +1,146 @@
+package datastructure.list.udemy;
+
+import java.util.Objects;
+
+public class EmployeeDoubleLinkedList {
+    private EmployeeNode head;
+    private EmployeeNode tail;
+    private int size;
+
+    public void addToFront(final Employee employee) {
+        var node = new EmployeeNode(employee);
+
+        if (head == null) {
+            tail = node;
+        } else {
+            head.setPrev(node);
+            node.setNext(head);
+        }
+
+        head = node;
+        size++;
+    }
+
+    public void addToEnd(final Employee employee) {
+        var node = new EmployeeNode(employee);
+
+        if (tail == null) {
+            head = node;
+        } else {
+            tail.setNext(node);
+            node.setPrev(tail);
+        }
+
+        tail = node;
+        size++;
+    }
+
+    public Employee removeFromFront() {
+        if (isEmpty()) {
+            return null;
+        }
+
+        var node = head;
+        if (head.next == null) {
+            head = null;
+            tail = null;
+        } else {
+            node.next.prev = null;
+            head = node.next;
+        }
+
+        size--;
+        return node.getEmployee();
+    }
+
+    public Employee removeFromEnd() {
+        if (isEmpty()) {
+            return null;
+        }
+
+        var node = tail;
+        if (tail.prev == null) {
+            tail = null;
+            head = null;
+        } else {
+            tail.prev.next = null;
+            tail = tail.prev;
+        }
+
+        size--;
+        return node.getEmployee();
+    }
+
+    public boolean isEmpty() {
+        return head == null;
+    }
+
+    public int size() {
+        return size;
+    }
+
+    @Override
+    public String toString() {
+        var builder = new StringBuilder("HEAD -> ");
+        var node = head;
+        while (node != null) {
+            builder.append(node).append("\t");
+            node = node.next;
+        }
+
+        return builder.toString();
+    }
+
+    private static final class EmployeeNode {
+        private EmployeeNode prev;
+        private Employee employee;
+        private EmployeeNode next;
+
+        public EmployeeNode(Employee employee) {
+            this.employee = employee;
+        }
+
+        public EmployeeNode getPrev() {
+            return prev;
+        }
+
+        public void setPrev(EmployeeNode prev) {
+            this.prev = prev;
+        }
+
+        public Employee getEmployee() {
+            return employee;
+        }
+
+        public void setEmployee(Employee employee) {
+            this.employee = employee;
+        }
+
+        public EmployeeNode getNext() {
+            return next;
+        }
+
+        public void setNext(EmployeeNode next) {
+            this.next = next;
+        }
+
+        @Override
+        public boolean equals(Object o) {
+            if (this == o) return true;
+            if (o == null || getClass() != o.getClass()) return false;
+            EmployeeNode that = (EmployeeNode) o;
+            return Objects.equals(prev, that.prev) && Objects.equals(employee, that.employee) && Objects.equals(next, that.next);
+        }
+
+        @Override
+        public int hashCode() {
+            return Objects.hash(prev, employee, next);
+        }
+
+        @Override
+        public String toString() {
+            return employee.toString();
+        }
+    }
+
+}
---
+++ src/datastructure/list/udemy/EmployeeDoubleLinkedListDriver.java
@@ -0,0 +1,49 @@
+package datastructure.list.udemy;
+
+public class EmployeeDoubleLinkedListDriver {
+
+    public static void main(String[] args) {
+        var 남동길 = new Employee("남동길", 1);
+        var 김철수 = new Employee("김철수", 2);
+        var 김영희 = new Employee("김영희", 3);
+
+        EmployeeDoubleLinkedList list = new EmployeeDoubleLinkedList();
+        System.out.println("list is empty : " + list.isEmpty() + "\t list size is : " + list.size());
+        list.addToFront(남동길);
+        System.out.println("list is empty : " + list.isEmpty() + "\t list size is : " + list.size());
+        list.addToFront(김철수);
+        System.out.println("list is empty : " + list.isEmpty() + "\t list size is : " + list.size());
+        list.addToEnd(김영희);
+        System.out.println("list is empty : " + list.isEmpty() + "\t list size is : " + list.size());
+
+        System.out.println(list);
+
+
+        Employee removed1 = list.removeFromEnd();
+        System.out.println("마지막 삭제");
+        System.out.println("삭제 :" + removed1);
+        System.out.println(list);
+        System.out.println("list is empty : " + list.isEmpty() + "\t list size is : " + list.size());
+
+        Employee removed2 = list.removeFromFront();
+        System.out.println("처음 삭제");
+        System.out.println("삭제 : " + removed2);
+        System.out.println(list);
+        System.out.println("list is empty : " + list.isEmpty() + "\t list size is : " + list.size());
+
+        Employee removed3 = list.removeFromFront();
+        System.out.println("처음 삭제");
+        System.out.println("삭제 : " + removed3);
+        System.out.println(list);
+        System.out.println("list is empty : " + list.isEmpty() + "\t list size is : " + list.size());
+
+
+        var 아무개 = new Employee("아무개", 4);
+        list.addToEnd(아무개);
+
+        System.out.println(list);
+        System.out.println("list is empty : " + list.isEmpty() + "\t list size is : " + list.size());
+
+
+    }
+}
