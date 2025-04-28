dedcc56162210831973113c50df181ead3ebb935
ndgndg91 <ndgndg91@gmail.com>
2025-04-28 23:50:50 +0900

    neet code top k frequent elements 2

--- app/src/main/java/datastructure/algorithms/algorithms/neetcode/TopKFrequentElements.java
+++ app/src/main/java/datastructure/algorithms/algorithms/neetcode/TopKFrequentElements.java
@@ -1,6 +1,8 @@
 package datastructure.algorithms.algorithms.neetcode;
 
+import java.util.ArrayList;
 import java.util.HashMap;
+import java.util.List;
 import java.util.Map;
 
 public class TopKFrequentElements {
@@ -24,5 +26,44 @@ public class TopKFrequentElements {
                 .limit(k)
                 .mapToInt(Map.Entry::getKey)
                 .toArray();
+    }
+
+    public int[] topKFrequent2(int[] nums, int k) {
+        // Step 1: Count frequency of each number
+        Map<Integer, Integer> frequencyMap = new HashMap<>();
+        for (int num : nums) {
+            frequencyMap.put(num, frequencyMap.getOrDefault(num, 0) + 1);
+        }
+
+        // Step 2: Create bucket sort array where index is frequency
+        // The maximum frequency possible is the length of the array
+        List<Integer>[] buckets = new ArrayList[nums.length + 1];
+        for (int i = 0; i < buckets.length; i++) {
+            buckets[i] = new ArrayList<>();
+        }
+
+        // Step 3: Put numbers in buckets based on their frequency
+        for (Map.Entry<Integer, Integer> entry : frequencyMap.entrySet()) {
+            int num = entry.getKey();
+            int frequency = entry.getValue();
+            buckets[frequency].add(num);
+        }
+
+        // Step 4: Get top k frequent elements by going from highest frequency buckets
+        int[] result = new int[k];
+        int resultIndex = 0;
+
+        for (int i = buckets.length - 1; i >= 0 && resultIndex < k; i--) {
+            List<Integer> bucket = buckets[i];
+            for (int num : bucket) {
+                if (resultIndex < k) {
+                    result[resultIndex++] = num;
+                } else {
+                    break;
+                }
+            }
+        }
+
+        return result;
     }
 }
